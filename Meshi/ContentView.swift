1e1a7d6ff93be7c9803a1be5023a639f70e7f8c0
S, Praveen (Cognizant) <praveen.s.contractor@pepsico.com>
2024-10-26 16:40:55 +0530

    Initial Commit

---
+++ Meshi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meshi
+//
+//  Created by S, Praveen (Cognizant) on 26/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meshi/MeshiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshiApp.swift
+//  Meshi
+//
+//  Created by S, Praveen (Cognizant) on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MeshiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
